31df411bfd90824a6d3433757e4a0fb44789fdb1
Daniel-SG <59225672+Daniel-SG@users.noreply.github.com>
2020-11-21 22:56:26 +0100

    Update scala_basics.scala

--- Spark/scala_basics.scala
+++ Spark/scala_basics.scala
@@ -263,3 +263,25 @@ val r = a.get("foo") match {
   // 1 - use final on member
   // 2 - use final on the entire class
   // 3 - seal the class = extend classes in THIS FILE, prevent extension in other files
+    
+    // traits vs abstract classes
+  // 1 - traits do not have constructor parameters
+  // 2 - multiple traits may be  inherited by the same class
+  // 3 - traits = behavior, abstract class = "thing"
+    // abstract
+  abstract class Animal {
+    val creatureType: String = "wild"
+    def eat: Unit
+  }
+     // traits
+  trait Carnivore {
+    def eat(animal: Animal): Unit
+    val preferredMeal: String = "fresh meat"
+  }
+
+  trait ColdBlooded
+  class Crocodile extends Animal with Carnivore with ColdBlooded {
+    override val creatureType: String = "croc"
+    def eat: Unit = println("nomnomnom")
+    def eat(animal: Animal): Unit = println(s"I'm a croc and I'm eating ${animal.creatureType}")
+  }
