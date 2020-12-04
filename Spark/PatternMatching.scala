577e0f7f66ba3e1fa7c30b6875b15d4c84cf2222
Daniel-SG <59225672+Daniel-SG@users.noreply.github.com>
2020-12-04 18:33:25 +0100

    Update PatternMatching.scala

--- Spark/PatternMatching.scala
+++ Spark/PatternMatching.scala
@@ -50,4 +50,41 @@ sealed class Animal
       case List(1,2,3) :+ 42 => // infix pattern
     }
   
+   // generators are also based on PATTERN MATCHING
+  val tuples = List((1,2), (3,4))
+  val filterTuples = for {
+    (first, second) <- tuples
+  } yield first * second
+  
+  
+  // big idea #3
+  val tuple = (1,2,3)
+  val (a, b, c) = tuple
+  println(b)
+  // multiple value definitions based on PATTERN MATCHING
+  // ALL THE POWER
+
+  val head :: tail = list
+  println(head)
+  println(tail)
+
+  // big idea #4 - NEW
+  // partial function based on PATTERN MATCHING
+  val mappedList = list.map {
+    case v if v % 2 == 0 => v + " is even"
+    case 1 => "the one"
+    case _ => "something else"
+  } // partial function literal
+  
+// both expressopns are the same
+  
+  val mappedList2 = list.map { x => x match {
+      case v if v % 2 == 0 => v + " is even"
+      case 1 => "the one"
+      case _ => "something else"
+    }
+  }
+  println(mappedList)
+  
+  
 }
